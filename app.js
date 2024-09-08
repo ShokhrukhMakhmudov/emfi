dafc783b2f8afffb1c8b7cbdbd13be1308bec2b9
ShokhrukhMakhmudov <msshohruh@gmail.com>
2024-09-08 16:42:25 +0500

    added token

--- app.js
+++ app.js
@@ -3,44 +3,39 @@ let openCardId = null;
 
 // Функция для получения сделок через API (максимум 3 за раз)
 async function fetchDeals() {
-  let page = 1; // предполагаем, что страницы сделок пронумерованы
-  const limit = 3;
+  console.log(1);
 
-  while (true) {
-    const response = await fetch(
-      "https://maxmoodoff13.amocrm.ru/api/v4/leads/",
-      {
-        method: "GET",
-        headers: {
-          Authorization:
-            "Basic " +
-            btoa("maxmoodoff13:6b2b0b9d-2d7c-4d9c-bb4a-0e5f7d4b0b7d"),
-        },
-      }
-    );
-    const deals = await response.json();
-    console.log(deals);
+  const res = await fetch(
+    "https://maxmoodoff13.amocrm.ru/api/v4/leads?limit=3",
+    {
+      cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImYyYmQ1MGMzNmMxMmI4YjFlMTU4M2UyZmQyZTg0NzQwZGQ5MGUzYTljNjkxY2QyNTExZmRiOThiMzk2NWI5Y2JiM2YwZmNhMzUzNDMxMjE4In0.eyJhdWQiOiJkNDFkZWVmNS1kYzBmLTQ4NDItYTJmMC1kZGZiMWEzOTJiYjkiLCJqdGkiOiJmMmJkNTBjMzZjMTJiOGIxZTE1ODNlMmZkMmU4NDc0MGRkOTBlM2E5YzY5MWNkMjUxMWZkYjk4YjM5NjViOWNiYjNmMGZjYTM1MzQzMTIxOCIsImlhdCI6MTcyNTc5NDc2NSwibmJmIjoxNzI1Nzk0NzY1LCJleHAiOjE3MjU4ODExNjUsInN1YiI6IjExNDkwNjA2IiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxOTM4NjEwLCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJwdXNoX25vdGlmaWNhdGlvbnMiLCJmaWxlcyIsImNybSIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiZmY0YjZlYjQtNWQ4NS00OThlLWFmYWEtNDM5YzY4NDA3NzUyIiwiYXBpX2RvbWFpbiI6ImFwaS1iLmFtb2NybS5ydSJ9.Fq1AmJ1HHeauYDJ4mhRFcHRxzF_tU7GQfSvuXyp7VxYAfTalS1jchmGghXbH5IpdyH2d-IPiNnr3X0u-XkX63_bQNpaZwfV1_xOzFNX9JeLr8DgOf0EjYeKBEySqYdUu9fXTRA9TPIyYoYPARqCNC3O71N7kkNxYe7u-Bpiay1u7rE6uSxfznGvY2pWyYj7eEd4MdsydP0HPZftV9bSGOKACtJ71WbEG7Jfgim65PqyqmLYVTnFjtL3jRMnnSZbBya8sQtHa-XG_wiYEF2bCBOXI3QnheCj10Ho4sG2Zil-HVZiDO1Jkp7TSTtXj4cHYFvZTAvoJpCd8AVcof2pDuA"}`,
+      },
+    }
+  );
+  console.log(res);
+  const {
+    _embedded: { leads: deals },
+  } = await res.json();
 
-    deals.forEach((deal) => {
-      const row = document.createElement("tr");
-      row.setAttribute("data-id", deal.id);
+  deals.forEach((deal) => {
+    const row = document.createElement("tr");
+    row.setAttribute("data-id", deal.id);
 
-      row.innerHTML = `
+    row.innerHTML = `
           <td>${deal.id}</td>
           <td>${deal.name}</td>
           <td>${deal.budget}</td>
         `;
 
-      row.addEventListener("click", () => loadDealDetails(row, deal.id));
+    row.addEventListener("click", () => loadDealDetails(row, deal.id));
 
-      dealsTable.appendChild(row);
-    });
+    dealsTable.appendChild(row);
+  });
 
-    page++;
-    if (deals.length < limit) break; // прерываем цикл, если карточек меньше лимита
-
-    await new Promise((resolve) => setTimeout(resolve, 1000)); // ждем 1 секунду перед следующим запросом
-  }
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // ждем 1 секунду перед следующим запросом
 }
 
 // Функция для получения деталей сделки при клике
@@ -94,4 +89,5 @@ function formatDate(dateString) {
 }
 
 // Инициализация получения данных
+
 fetchDeals();
