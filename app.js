95166b3db9d7866a5c4b07f92d9d48a7de502607
ShokhrukhMakhmudov <msshohruh@gmail.com>
2024-09-08 17:00:07 +0500

    mode no-cors

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ async function fetchDeals() {
   const res = await fetch(
     "https://maxmoodoff13.amocrm.ru/api/v4/leads?limit=3",
     {
-      cache: "no-store",
+      method: "GET",
+      mode: "no-cors",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImYyYmQ1MGMzNmMxMmI4YjFlMTU4M2UyZmQyZTg0NzQwZGQ5MGUzYTljNjkxY2QyNTExZmRiOThiMzk2NWI5Y2JiM2YwZmNhMzUzNDMxMjE4In0.eyJhdWQiOiJkNDFkZWVmNS1kYzBmLTQ4NDItYTJmMC1kZGZiMWEzOTJiYjkiLCJqdGkiOiJmMmJkNTBjMzZjMTJiOGIxZTE1ODNlMmZkMmU4NDc0MGRkOTBlM2E5YzY5MWNkMjUxMWZkYjk4YjM5NjViOWNiYjNmMGZjYTM1MzQzMTIxOCIsImlhdCI6MTcyNTc5NDc2NSwibmJmIjoxNzI1Nzk0NzY1LCJleHAiOjE3MjU4ODExNjUsInN1YiI6IjExNDkwNjA2IiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxOTM4NjEwLCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJwdXNoX25vdGlmaWNhdGlvbnMiLCJmaWxlcyIsImNybSIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiZmY0YjZlYjQtNWQ4NS00OThlLWFmYWEtNDM5YzY4NDA3NzUyIiwiYXBpX2RvbWFpbiI6ImFwaS1iLmFtb2NybS5ydSJ9.Fq1AmJ1HHeauYDJ4mhRFcHRxzF_tU7GQfSvuXyp7VxYAfTalS1jchmGghXbH5IpdyH2d-IPiNnr3X0u-XkX63_bQNpaZwfV1_xOzFNX9JeLr8DgOf0EjYeKBEySqYdUu9fXTRA9TPIyYoYPARqCNC3O71N7kkNxYe7u-Bpiay1u7rE6uSxfznGvY2pWyYj7eEd4MdsydP0HPZftV9bSGOKACtJ71WbEG7Jfgim65PqyqmLYVTnFjtL3jRMnnSZbBya8sQtHa-XG_wiYEF2bCBOXI3QnheCj10Ho4sG2Zil-HVZiDO1Jkp7TSTtXj4cHYFvZTAvoJpCd8AVcof2pDuA"}`,
@@ -55,7 +56,9 @@ async function loadDealDetails(row, dealId) {
 
   row.innerHTML = '<td colspan="3"><div class="spinner"></div></td>';
 
-  const response = await fetch(`https://api.example.com/deals/${dealId}`);
+  const response = await fetch(
+    `https://maxmoodoff13.amocrm.ru/api/v4/leads/${dealId}`
+  );
   const deal = await response.json();
 
   const taskStatus = getTaskStatus(deal.task_date); // Определяем статус задачи
@@ -89,5 +92,4 @@ function formatDate(dateString) {
 }
 
 // Инициализация получения данных
-
 fetchDeals();
